edbae474a3f3ce0b1eed88a47ee26eb36916770d
itsSamarIbrahim <>
2023-11-28 11:18:28 -0800

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,6 @@
 def print_last_digit(number):
     if (number < 0):
         number = -number
-    last_digit = number %10
+    last_digit = number % 10
     print(last_digit, end='')
     return (last_digit)
