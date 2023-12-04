31662115361dd847c13c334accb766c88ca25cfd
itsSamarIbrahim <>
2023-12-04 14:06:48 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
-    if (sentence == 0):
+    if (length == 0):
         return (length, None)
     else:
         return (length, first)
