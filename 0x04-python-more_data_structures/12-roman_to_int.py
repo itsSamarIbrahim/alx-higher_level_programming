133ad4d78132072cc434a073bc569f19292bc6b0
itsSamarIbrahim <>
2023-12-06 13:00:06 -0800

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return (0)
+
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+        }
+
+    final_value = 0
+    previous_value = 0
+
+    for letter in reversed(roman_string):
+        value = roman_dict[letter]
+        if value < previous_value:
+            final_value = final_value - value
+        else:
+            final_value = final_value + value
+        previous_value = value
+
+    return (final_value)
