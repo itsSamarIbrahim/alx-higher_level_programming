e72d7160a3cf08cfba58b930dff9d0468a2c5864
itsSamarIbrahim <>
2023-12-06 12:22:05 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_value = 0
-    max_key = None  # it is okay to use None here
+    max_key = None
     for key, value in a_dictionary.items():
         if value > max_value:
             max_value = value
