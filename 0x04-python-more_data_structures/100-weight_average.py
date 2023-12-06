e2dcb914b44337ba025d6c6ae69930ed4f68d376
itsSamarIbrahim <>
2023-12-06 13:15:19 -0800

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return (0)
+    total_score = 0
+    total_weight = 0
+    for score, weight in my_list:
+        total_score = total_score + (score * weight)
+        total_weight = total_weight + weight
+    result = total_score / total_weight
+
+    return (result)
