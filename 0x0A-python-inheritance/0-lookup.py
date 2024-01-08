216c1f00567ddde0d61146b74fff34727b69e9ba
itsSamarIbrahim <>
2024-01-08 12:21:53 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,12 +2,14 @@
 
 def lookup(obj):
     """
-    This function takes an object as input and returns a list of valid attributes and methods for that object.
+    This function takes an object as input and returns a list of
+    valid attributes and methods for that object.
 
     Args:
     obj: Any Python object.
 
     Returns:
-    A list of strings representing the valid attributes and methods of the input object.
+    A list of strings representing the valid attributes
+    and methods of the input object.
     """
     return dir(obj)
