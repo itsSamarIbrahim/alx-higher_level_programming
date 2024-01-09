2d54012f4440accc3f94ba50f004741a66bfc439
itsSamarIbrahim <>
2024-01-09 14:45:15 -0800

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,10 +8,12 @@ import json
 
 def load_from_json_file(filename):
     """
-    Load a JSON string from the specified file and convert it to a Python object.
+    Load a JSON string from the specified file and convert it
+    to a Python object.
 
     Args:
-    filename (str): The name of the file from which the JSON string will be loaded.
+    filename (str): The name of the file from which the JSON string
+    will be loaded.
 
     Returns:
     object: The Python object represented by the JSON string in the file.
