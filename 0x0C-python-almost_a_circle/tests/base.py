329263ad3372861dd226f1cc62d3ab4c8b72c6a1
itsSamarIbrahim <>
2024-01-15 14:51:42 -0800

    tests

---
+++ 0x0C-python-almost_a_circle/tests/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+
+if __name__ == '__main__':
+    unittest.main()
