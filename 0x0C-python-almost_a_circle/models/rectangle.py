f19260c80374ae9330cd9dbf2bd8d14ead9a1de9
itsSamarIbrahim <>
2024-01-15 07:41:48 -0800

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -45,10 +45,10 @@ class Rectangle(Base):
             The unique identifier of the rectangle.
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
