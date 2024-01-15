72bc343f60048035955f5021b8a63285b90f69ca
itsSamarIbrahim <>
2024-01-15 12:02:42 -0800

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 model-1
 """
+import json
 
 
 class Base:
@@ -23,3 +24,21 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list):
+            A list of dictionaries to be converted to JSON.
+
+        Returns:
+            str:
+            A JSON string representation of the input list of dictionaries.
+            If the input list is None or empty, returns "[]".
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
