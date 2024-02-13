07201288919ba0f7d70eb3828857b199047de43a
itsSamarIbrahim <>
2024-02-13 14:57:11 -0800

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
--- This is a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending) by importing table_dump_1.sql table in hbtn_0c_0 database
+-- This is a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending) by importing table_dump.sql table in hbtn_0c_0 database
 SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
