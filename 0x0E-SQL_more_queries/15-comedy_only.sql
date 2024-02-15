2b073ed6377826370bd16c8a3018a17993824911
itsSamarIbrahim <>
2024-02-14 18:49:10 -0800

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -7,6 +7,6 @@
 --    The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genresON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY 1 ASC;
