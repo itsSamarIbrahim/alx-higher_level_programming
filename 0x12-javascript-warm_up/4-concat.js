b4c9081c41999524e72144bf9fbea13201d33daa
itsSamarIbrahim <>
2024-03-11 08:54:28 -0700

    added

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
 
-const [arg1, arg2] = process.argv.slice(2);
-
-if (arg1 === undefined) {
-  arg1 = 'undefined';
-}
-
-if (arg2 === undefined) {
-  arg2 = 'undefined';
-}
-
-console.log(`${arg1} is ${arg2}`);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
