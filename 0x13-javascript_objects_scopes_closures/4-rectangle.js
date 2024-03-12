6e9b5e5a3943b4003802dfa348200b9d9ae5b8fd
itsSamarIbrahim <>
2024-03-12 16:01:16 -0700

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -10,7 +10,6 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      const temp = [];
       for (let j = 0; j < this.width; j++) {
         process.stdout.write('X');
       }
