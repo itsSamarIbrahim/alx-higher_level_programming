52dca6046c31411bee535f5b4cfaf9a2091a798f
itsSamarIbrahim <>
2024-03-27 18:07:53 -0700

    0-body_size.sh

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#A Bash script that takes in a URL, sends a request to that URL,
+#and displays the size of the body of the response
+
+#	The size must be displayed in bytes
+#	You have to use curl
+#Please test your script in the sandbox provided,
+#using the web server running on port 5000
+
+curl -s "$1" | wc -c
