a688e496bce6ed5fd7714a5322181623d1f46023
itsSamarIbrahim <>
2024-03-28 20:02:15 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     req = requests.get(url)
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
-    else
+    else:
         print(req.text)
