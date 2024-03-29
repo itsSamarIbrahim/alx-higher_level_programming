f7e5f8aa6f144bd52c68edca4fba6322558e7edb
itsSamarIbrahim <>
2024-03-28 19:11:36 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,9 +8,9 @@ import requests
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     letter = sys.argv[1]
-    params = {'q': letter}
-    response = requests.post(url, data=params)
     if len(sys.argv) > 1:
+        params = {'q': letter}
+        response = requests.post(url, data=params)
         try:
             data = response.json()
             if data:
