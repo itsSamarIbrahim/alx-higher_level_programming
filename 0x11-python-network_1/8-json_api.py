3a3d7322dc895a0a99bc9b6da26d0c18d6fa9ddf
itsSamarIbrahim <>
2024-03-28 19:23:46 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         letter = ""
     else:
-        sys.argv[1]
+        letter = sys.argv[1]
     url = "http://0.0.0.0:5000/search_user"
     letter = sys.argv[1]
     params = {'q': letter}
