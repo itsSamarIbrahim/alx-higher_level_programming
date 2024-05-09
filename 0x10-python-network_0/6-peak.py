b99e65132632489a14b930dc50c5d15331a72a3c
Samar <bantmasr21@gmail.com>
2024-05-09 15:47:25 -0700

    6-peak.py & 6-peak.txt

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -16,17 +16,28 @@ def find_peak(list_of_integers):
 
     size = len(list_of_integers)
 
-    if size == 1:
+    if size == 1 or list_of_integers[0] >= list_of_integers[1]:
         return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    elif list_of_integers[-1] >= list_of_integers[-2]:
+        return list_of_integers[-1]
 
-    mid = int(size/2)
-    peak = list_of_integers[mid]
-
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
+    mid = size // 2
+    if list_of_integers[mid] < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
     else:
-        return find_peak(list_of_integers[mid + 1:])
+        return find_peak(list_of_integers[mid:])
+
+#    if size == 1:
+#        return list_of_integers[0]
+#    elif size == 2:
+#        return max(list_of_integers)
+#
+#    mid = int(size/2)
+#    peak = list_of_integers[mid]
+#
+#    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+#        return peak
+#    elif peak < list_of_integers[mid - 1]:
+#        return find_peak(list_of_integers[:mid])
+#    else:
+#        return find_peak(list_of_integers[mid + 1:])
