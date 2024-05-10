8ed9621b87f97b13069b2bb58f67106c4976e901
Samar <bantmasr21@gmail.com>
2024-05-10 14:56:48 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -17,10 +17,10 @@ if __name__ == '__main__':
 
 # import sys
 # import urllib.request
-# 
+#
 # if __name__ == "__main__":
 #     url = sys.argv[1]
-# 
+#
 #     request = urllib.request.Request(url)
 #     with urllib.request.urlopen(request) as response:
 #         print(dict(response.headers).get("X-Request-Id"))
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the
+email as a parameter, and displays the body of
+the response (decoded in utf-8)
+"""
+from sys import argv
+from urllib import request, parse
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = parse.urlencode(value).encode("ascii")
+
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
