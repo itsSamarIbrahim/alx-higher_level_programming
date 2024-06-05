9790a3f2f1bda963b4d6111aaf5889a11324537c
Samar <bantmasr21@gmail.com>
2024-06-04 17:07:31 -0700

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* a script that writes a string to a file */
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
