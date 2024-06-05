a093cc359b3dd65fef5ec1da86fb77409178e0f9
Samar <bantmasr21@gmail.com>
2024-06-04 18:13:00 -0700

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/* a script that gets the contents of a webpage and stores it in a file */
+const request = require('request');
+
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
