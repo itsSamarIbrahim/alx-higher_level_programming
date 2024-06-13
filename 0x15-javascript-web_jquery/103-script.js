8c53e4699873a0ec8cd3364cb9fb573922adfd93
Samar <bantmasr21@gmail.com>
2024-06-12 17:56:21 -0700

    103-script.js

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#btn_translate').click(fetchTranslation);
+
+  $('#language_code').keypress(function(event) {
+    if (event.which === 13) {
+      fetchTranslation();
+    }
+  });
+
+  function fetchTranslation() {
+    const languageCode = $('#language_code').val();
+    const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`;
+
+    $.get(apiUrl, function(response) {
+      $('#hello').text(response.hello);
+    });
+  }
+});
